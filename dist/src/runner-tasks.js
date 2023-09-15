1366cd2137146dfa4895073bba8f5d42c531912b
aksoni27 <ankitksoni1997@gmail.com>
2023-09-16 02:30:45 +0530

    chore: removed dist from repository
    r

--- dist/src/runner-tasks.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.taskProviders = void 0;
-const TestTask_service_1 = require("./tasks/TestTask.service");
-exports.taskProviders = [TestTask_service_1.TestTask, TestTask_service_1.Test2Task];
